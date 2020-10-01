4ff96b1545456b0bbf0e657b951f0be4f3dd008d
pi-squared-4 <66795444+pi-squared-4@users.noreply.github.com>
2020-10-01 13:14:34 +0530

    Game index.js added

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+   document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
+} 
+else{
+   document.querySelector("h1").innerHTML="Draw!";    
+}
+
